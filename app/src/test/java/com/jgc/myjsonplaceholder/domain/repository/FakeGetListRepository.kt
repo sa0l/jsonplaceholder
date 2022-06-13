7a59603b43f5ee9220307c0fb2d3f5dd40af4e65
krzysztof.martys <krzysztof.martys@billennium.com>
2022-06-13 19:52:29 +0200

    remove redundant files

--- app/src/test/java/com/jgc/myjsonplaceholder/domain/repository/FakeGetListRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.jgc.myjsonplaceholder.domain.repository
-
-import com.jgc.myjsonplaceholder.domain.models.Post
-import com.jgc.myjsonplaceholder.domain.repository.list.ListRepository
-
-class FakeGetListRepository: ListRepository {
-    override suspend fun getPostList(): List<Post> = emptyList()
-}
